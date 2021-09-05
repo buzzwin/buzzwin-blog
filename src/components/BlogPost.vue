a82597c4e2810937f270a48c72e517a4c2272895
Gunjan <gunjanv@yahoo.com>
2021-09-04 20:41:51 -0500

    Fixed hard coded values again

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -4,11 +4,8 @@
       <header class="post__header">
         <h2 class="post__title">{{ title }}</h2>
 
-        <h3 class="post__meta">
-          by
-          <router-link class="post__author" :to="`/by/${kebabify(author)}`">{{
-            author
-          }}</router-link>
+        <h3 class="post__meta">by <router-link class="post__author"
+          :to="`/by/${kebabify(author)}`">{{ author }}</router-link>
           <span class="post__sep"></span>
           <time>{{ prettyDate(published) }}</time>
         </h3>
@@ -19,46 +16,42 @@
       <section class="post__body rte" v-html="content"></section>
 
       <footer class="post__footer">
-        <vue-disqus
-          v-if="commentsReady"
-          shortname="buzzwin"
-          :key="post"
-          :identifier="post"
-          :url="`https://buzzwin.com/read/${post}`"
-        />
+        <vue-disqus v-if="commentsReady" shortname="vue-blog-demo"
+          :key="post" :identifier="post" :url="`https://vue-blog-demo.netlify.com/read/${post}`"/>
       </footer>
     </article>
   </transition>
 </template>
 
 <script>
-import VueDisqus from "vue-disqus/VueDisqus";
-import { kebabify, prettyDate } from "../helpers";
+import VueDisqus from 'vue-disqus/VueDisqus'
+import { kebabify, prettyDate } from '../helpers'
 
 export default {
-  name: "blog-post",
-  resource: "BlogPost",
+  name: 'blog-post',
+  resource: 'BlogPost',
   components: { VueDisqus },
   props: { post: String },
 
   data() {
     return {
-      title: "",
-      author: "",
-      content: "",
-      published: "",
-      description: "",
-      commentsReady: false,
-    };
+      title: '',
+      author: '',
+      content: '',
+      published: '',
+      description: '',
+      commentsReady: false
+    }
   },
 
   watch: {
     post(to, from) {
       if (to === from || !this.post) return;
 
-      this.commentsReady = false;
-      this.$getResource("post", to).then(this.showComments);
-    },
+      this.commentsReady = false
+      this.$getResource('post', to)
+        .then(this.showComments)
+    }
   },
 
   methods: {
@@ -66,14 +59,15 @@ export default {
     prettyDate,
     showComments() {
       setTimeout(() => {
-        this.commentsReady = true;
-      }, 1000);
-    },
+        this.commentsReady = true
+      }, 1000)
+    }
   },
 
   beforeMount() {
     if (!this.post) return;
-    this.$getResource("post", this.post).then(this.showComments);
-  },
-};
+    this.$getResource('post', this.post)
+      .then(this.showComments)
+  }
+}
 </script>
